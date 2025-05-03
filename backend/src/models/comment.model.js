fb561c30e2dc9987efdb54ed20835915820d5806
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-03 16:11:08 +0200

    Create a model for comment

---
+++ backend/src/models/comment.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    comment: {
+        type: String,
+        required: true
+    },
+    //  The user that post the comment and ref to reference de user from User(user.model.js)
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Blog",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+
+}, {
+    timestamps: true
+})
+
+const Comment = mongoose.model("Comment", commentSchema)
+export default Comment;

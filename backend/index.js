be51ba9664794abb176d12122b0674ca80e92662
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-03 11:39:29 +0200

    Connect MongoDB and Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 import express from "express";
+dotenv.config();
 const app = express()
 const port = process.env.PORT || 5000
+
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connexion reussi')
+}).catch((err) => {
+    console.log('Connection non reussi', err)
+})
 
 
 app.get('/', (req, res) => {

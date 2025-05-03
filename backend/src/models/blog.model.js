882319d8934461a9aed6a17252c5e01934996651
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-03 12:25:50 +0200

    Create a blog model using mongoose

---
+++ backend/src/models/blog.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+// Modify after
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    content: String,
+    coverImg: String,
+    category: String,
+    author: String,
+    rating: Number,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+export default Blog

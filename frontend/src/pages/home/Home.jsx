ce04c497733dc0c235a5dc66443bfef5da221071
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-05 13:35:37 +0200

    Create Blog and Search Component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import Hero from "./Hero"
+import Blogs from "../blogs/Blogs"
 
 const Home = () => {
     return (
         <div className="bg-white text-primary container mx-auto max-w-6xl mt-8 p-8">
             <Hero />
-            <div>Blogs</div>
+            <Blogs />
         </div>
     )
 }

10962249e0ca65251fd78b275a34ce2f82961cfb
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-05 10:45:42 +0200

    Make navbar mobile responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom"
+import { IoMenuSharp } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 const navList = [
     { name: "Home", path: "/" },
     { name: "About us", path: "/about-us" },
@@ -8,9 +10,11 @@ const navList = [
 ]
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
     return (
         <header className='bg-white py-6 border'>
-            <nav className='container mx-auto flex justify-between px-5 max-w-6xl'>
+            <nav className='container mx-auto flex justify-between items-center px-5 max-w-6xl'>
                 <a href="/">
                     <img src="/logo.png" alt="" className='h-12' />
                 </a>
@@ -34,7 +38,44 @@ const Navbar = () => {
                     </li>
 
                 </ul>
+                {/* toggle menu */}
+                <div className='flex items-center sm:hidden'>
+
+                    <button onClick={toggleMenu} className="flex items-center px-3 py-4 bg-[#fafafa] 
+                    rounded text-sm text-gray-500 hover:text-gray-900">
+                        {
+                            isMenuOpen ? <IoClose className='size-10' /> : <IoMenuSharp className='size-10' />
+                        }
+                    </button>
+                </div>
             </nav>
+            {/* mobile menu items */}
+            {
+                isMenuOpen && (
+                    <ul className='fixed top-[108px] left-0 w-full h-auto pb-8 border-b bg-white shadow-sm z-50'>
+                        {
+                            navList.map((list, index) => (
+                                <li className='mt-5 px-4'>
+                                    <NavLink
+                                        onClick={() => setIsMenuOpen(false)}
+                                        to={`${list.path}`}
+                                        className={({ isActive }) =>
+                                            isActive ? "active" : ""
+                                        }
+                                    >
+                                        {list.name}
+                                    </NavLink>
+                                </li>
+                            ))
+                        }
+
+                        <li className='px-4 mt-5'>
+                            <NavLink to="/login">Login</NavLink>
+                        </li>
+
+                    </ul>
+                )
+            }
         </header>
     )
 }

356308e39192f9eeae8e4fd025c8ebb1bf3234aa
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-07 10:23:41 +0200

    Auth Api Setup

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { blogApi } from './features/blogs/blogApis'
+import authApi from './features/auth/authApi'
 
 export const store = configureStore({
     reducer: {
-        [blogApi.reducerPath]: blogApi.reducer
+        [blogApi.reducerPath]: blogApi.reducer,
+        [authApi.reducerPath]: authApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(blogApi.middleware),

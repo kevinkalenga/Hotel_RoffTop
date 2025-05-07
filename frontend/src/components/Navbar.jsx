8e526b1df49acbb0d3c6398d4e6df5c968fb9b14
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-08 01:53:03 +0200

    Modify and Show User profile on Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import { IoMenuSharp } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
 import { useSelector, useDispatch } from 'react-redux';
+import avatarImg from "../assets/commentor.png"
+import { useLogoutUserMutation } from '../redux/features/userAuth/userAuthApi';
+import { logout } from '../redux/features/userAuth/authSlice';
 const navList = [
     { name: "Home", path: "/" },
     { name: "About us", path: "/about-us" },
@@ -15,6 +18,21 @@ const Navbar = () => {
     const { user } = useSelector((state) => state.auth);
     console.log(user)
     const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+    const dispatch = useDispatch()
+
+    const [logoutUser] = useLogoutUserMutation()
+
+    const handleLogout = async () => {
+        try {
+            await logoutUser().unwrap();
+            dispatch(logout())
+        } catch (error) {
+
+        }
+    }
+
+
+
     return (
         <header className='bg-white py-6 border'>
             <nav className='container mx-auto flex justify-between items-center px-5 max-w-6xl'>
@@ -36,18 +54,46 @@ const Navbar = () => {
                         ))
                     }
 
-                    <li>
-                        <NavLink to="/login">Login</NavLink>
-                    </li>
+                    {/* render btn based on user login activity */}
+                    {
+                        user && user.role === "user" ? (<li className='flex items-center gap-3'>
+                            <img src={avatarImg} alt="" className='size-8' />
+
+                            <button
+
+                                onClick={handleLogout}
+                                className='bg-[#1E73BE] px-4 py-1.5 text-white rounded-sm'>
+                                <Link to="/">
+                                    Logout
+                                </Link>
+
+                            </button>
+
+                        </li>) : (
+                            <li>
+                                <NavLink to="/login">Login</NavLink>
+                            </li>
+                        )
+                    }
+                    {
+                        user && user.role === "admin" && (<li className='flex items-center gap-3'>
+                            <img src={avatarImg} alt="" className='size-8' />
+                            <Link to="/dashboard">
+                                <button className='bg-[#1E73BE] px-4 py-1.5 text-white rounded-sm'>Dashboard</button>
+                            </Link>
+                        </li>)
+                    }
+
+
 
                 </ul>
                 {/* toggle menu */}
                 <div className='flex items-center sm:hidden'>
 
-                    <button onClick={toggleMenu} className="flex items-center px-3 py-4 bg-[#fafafa] 
+                    <button onClick={toggleMenu} className="flex items-center px-3 py-4 bg-[#fafafa]
                     rounded text-sm text-gray-500 hover:text-gray-900">
                         {
-                            isMenuOpen ? <IoClose className='size-10' /> : <IoMenuSharp className='size-10' />
+                            isMenuOpen ? <IoClose className='size-6' /> : <IoMenuSharp className='size-6' />
                         }
                     </button>
                 </div>

4d1c4b7097b01536b76a30ece7ee2a67ef313755
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-09 18:12:10 +0200

    Modify blog API

--- frontend/src/redux/features/blogs/blogApis.js
+++ frontend/src/redux/features/blogs/blogApis.js
@@ -7,21 +7,57 @@ export const blogApi = createApi({
     reducerPath: "blogsApi",
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:5000/api/',
+        credentials: 'include'
 
     }),
+    tagTypes: ['Blogs'],
     endpoints: (builder) => (
         {
             fetchBlogs: builder.query({
-                query: ({ search = '', category = '', location = '' }) => `/blogs?search=${search}&category=${category}&location=${location}`
+                query: ({ search = '', category = '', location = '' }) => `/blogs?search=${search}&category=${category}&location=${location}`,
+                providesTags: ['Blogs']
             }),
             fetchBlogById: builder.query({
                 query: (id) => `/blogs/${id}`
             }),
             fetchRelatedBlogs: builder.query({
                 query: (id) => `/blogs/related/${id}`
+            }),
+            postBlog: builder.mutation({
+                query: (newBlog) => ({
+                    url: `/blog/create-post`,
+                    method: "POST",
+                    body: newBlog,
+                    credentials: "include"
+                })
+            }),
+            updateBlog: builder.mutation({
+                query: ({ id, ...rest }) => ({
+                    url: `/blogs/update-post/${id}`,
+                    method: "PATCH",
+                    body: rest,
+                    credentials: "include"
+                }),
+                invalidatesTags: (result, error, { id }) => [{ type: 'Blogs', id }],
+            }),
+
+            deleteBlog: builder.mutation({
+                query: (id) => ({
+                    url: `/blogs/${id}`,
+                    method: "DELETE",
+                    credentials: "include"
+                }),
+                invalidatesTags: (result, error, { id }) => [{ type: 'Blogs', id }],
             })
         }
     )
 })
 
-export const { useFetchBlogsQuery, useFetchBlogByIdQuery, useFetchRelatedBlogsQuery } = blogApi
+export const {
+    useFetchBlogsQuery,
+    useFetchBlogByIdQuery,
+    useFetchRelatedBlogsQuery,
+    usePostBlogMutation,
+    useUpdateBlogMutation,
+    useDeleteBlogMutation
+} = blogApi

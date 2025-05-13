5fdb82f5e54051c75c3eef0d45d17ab84719c15b
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-13 13:19:41 +0200

    Manage User and Update user role

--- frontend/src/pages/user/Register.jsx
+++ frontend/src/pages/user/Register.jsx
@@ -1,44 +1,94 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { useRegisterUserMutation } from "../../redux/features/userAuth/userAuthApi";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Register = () => {
 
-    const [username, setUserName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
-    const [message, setMessage] = useState('')
+    const [username, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [password, setPassword] = useState("");
+    const [registerUser, { isLoading }] = useRegisterUserMutation();
+
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        e.preventDefault();
+        const data = {
+            username,
+            email,
+            password,
+        };
+
+        console.log(data);
+        try {
+            await registerUser(data).unwrap();
+            alert("Registration successful");
+            navigate('/login');
+        } catch (err) {
+            alert("Registration failed");
+        }
+    };
+
 
 
     return (
-        <div className='max-w-sm bg-white mx-auto p-8 mt-36'>
-            <h2 className='text-2xl font-semibold pt-5'>Please Register</h2>
-            <form className='space-y-5 max-w-sm mx-auto pt-8'>
-                <input type="text" value={username}
-                    placeholder="Username" required
+        <div className="max-w-sm bg-white mx-auto p-8 mt-36">
+            <h2 className="text-2xl font-semibold pt-5">Please Register</h2>
+            <form
+                onSubmit={handleRegister}
+                className="space-y-5 max-w-sm mx-auto pt-8"
+            >
+                <input
+                    type="text"
+                    value={username}
                     className="w-full bg-bgPrimary focus:outline-none px-5 py-3"
+                    onChange={(e) => setUserName(e.target.value)}
+                    placeholder="Username"
+                    required
                 />
-                <input type="text" value={email}
-                    placeholder="Email" required
+
+                <input
+                    type="text"
+                    value={email}
                     className="w-full bg-bgPrimary focus:outline-none px-5 py-3"
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
                 />
-                <input type="password" value={password}
-                    placeholder="Password" required
+
+                <input
+                    type="password"
+                    value={password}
                     className="w-full bg-bgPrimary focus:outline-none px-5 py-3"
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required
                 />
                 {
-                    message && <p className="text-red-500">{message}</p>
+                    message && <p className="text-red-500">{message}</p> // Display error message if any
                 }
-                <button className="w-full mt-5 bg-primary hover:bg-indigo-500 text-white 
-                   font-medium rounded-md py-1">
-                    Login
+                <button
+                    type="submit"
+                    disabled={isLoading}
+                    className="w-full mt-5 bg-primary hover:bg-indigo-500 text-white font-medium py-3 rounded-md"
+                >
+                    Register
                 </button>
             </form>
-            <p className="my-5 text-center">
-                Already have an account? Please <Link to="/login" className="text-red-700 italic"> Login</Link>
-            </p>
 
+            <p className="my-5 text-center">
+                Already have an account? Please
+                <Link to="/login" className="text-red-700 italic">
+                    {" "}
+                    Login
+                </Link>
+
+            </p>
         </div>
+
     )
 }
 
